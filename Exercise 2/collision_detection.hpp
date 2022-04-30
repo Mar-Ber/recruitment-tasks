ab3012094d9f38e770c16ebdafbdefa904afc96c
Marcin Bereźnicki <mbereznicki@student.agh.edu.pl>
2022-04-30 18:14:32 +0200

    Delete Exercise 2 directory

--- Exercise 2/collision_detection.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef EXERCISE_2_COLLISION_DETECTION_HPP
-#define EXERCISE_2_COLLISION_DETECTION_HPP
-
-#include <cmath>
-
-bool isCircleRectangleColliding(float circle_x, float circle_y, float radius,
-                                float rectangle_x, float rectangle_y, float rectangleWidth, float rectangleHeight);
-
-#endif //EXERCISE_2_COLLISION_DETECTION_HPP

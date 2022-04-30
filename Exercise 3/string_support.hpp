23fa680d6f9852528276636864d5e97415669c98
Marcin Bereźnicki <mbereznicki@student.agh.edu.pl>
2022-04-30 18:09:27 +0200

    Add files via upload

---
+++ Exercise 3/string_support.hpp
@@ -0,0 +1,10 @@
+#ifndef EXERCISE_3_STRING_SUPPORT_5PP
+#define EXERCISE_3_STRING_SUPPORT_5PP
+#include <string>
+#include <cstring>
+#include <iostream>
+
+std::string trim(const std::string &s);
+char* trim(char *s);
+
+#endif //EXERCISE_3_STRING_SUPPORT_5PP
